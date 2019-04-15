8bb3a9384d5d27dc3f3dd29de6f25eb679938923
André <andrelbkunde@gmail.com>
2019-04-15 18:10:40 -0300

    Frontend funcionando

--- src/pages/Box/index.js
+++ src/pages/Box/index.js
@@ -3,6 +3,7 @@ import api from "../../services/api";
 import { distanceInWords } from "date-fns";
 import pt from "date-fns/locale/pt";
 import Dropzone from "react-dropzone";
+import socket from "socket.io-client";
 
 import { MdInsertDriveFile } from "react-icons/md";
 
@@ -12,11 +13,26 @@ import "./styles.css";
 export default class Box extends Component {
   state = { box: {} };
   async componentDidMount() {
+    this.subscribeToNewFiles();
+
     const box = this.props.match.params.id;
     const response = await api.get(`boxes/${box}`);
 
     this.setState({ box: response.data });
   }
+
+  subscribeToNewFiles = () => {
+    const box = this.props.match.params.id;
+    const io = socket("https://week6-mobile-backend.herokuapp.com/");
+
+    io.emit("connectRoom", box);
+
+    io.on("file", data => {
+      this.setState({
+        box: { ...this.state.box, files: [data, ...this.state.box.files] }
+      });
+    });
+  };
 
   handleUpload = files => {
     files.forEach(file => {
@@ -50,7 +66,12 @@ export default class Box extends Component {
           {this.state.box.files &&
             this.state.box.files.map(file => (
               <li key={file._id}>
-                <a className="fileInfo" href={file.url} target="_blank">
+                <a
+                  className="fileInfo"
+                  href={file.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   <MdInsertDriveFile size={24} color="#A5Cfff" />
                   <strong>{file.title}</strong>
                 </a>
